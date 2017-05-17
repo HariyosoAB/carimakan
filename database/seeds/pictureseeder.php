b311dd0fc177594e40dec7b6163574da85a07d9b
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-18 02:39:24 +0700

    picture seeder

--- database/seeds/pictureseeder.php
+++ database/seeds/pictureseeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class pictureseeder extends Seeder
 {
@@ -11,6 +12,40 @@ class pictureseeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('picture')->insert([
+    		[
+    				'Picture_Resto' => 3,
+    				'Picture_Filename' => 1,
+    		],
+    		[
+    				'Picture_Resto' => 3,
+    				'Picture_Filename' => 2,
+    		],
+    		[
+    				'Picture_Resto' => 4,
+    				'Picture_Filename' => 8,
+    		],
+    		[
+    				'Picture_Resto' => 5,
+    				'Picture_Filename' => 3,
+    		],
+    		[
+    				'Picture_Resto' => 5,
+    				'Picture_Filename' => 4,
+    		],
+    		[
+    				'Picture_Resto' => 5,
+    				'Picture_Filename' => 5,
+    		],
+    		[
+    				'Picture_Resto' => 6,
+    				'Picture_Filename' => 6,
+    		],
+    		[
+    				'Picture_Resto' => 7,
+    				'Picture_Filename' => 7,
+    		],
+
+    	]);
     }
 }
